6a9abdab9b73775c61f615994efb4ae77dfff3fe
An <nguyentruongan28074@gmail.com>
2022-10-28 15:38:43 +0700

    ssss

--- app.js
+++ app.js
@@ -52,6 +52,11 @@ app.get("/upload", (req, res) => {
   res.render("upload")
 })
 
+// An tron
+app.get("/upload", (req, res) => {
+  res.render("upload")
+})
+
 app.post("/upload", upload.single('img'), (req, res) => {
     console.log(req.file);
     console.log(req.body);
